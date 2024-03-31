dfbf4801782e8b0276df6016747f679a617f3188
name <sarkeshnandu@gmail.com>
2024-03-31 19:29:01 +0530

    Functions added

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-#[allow(dead_code)]
 enum Type{
     F(String),
     C(String),
 }
 
+fn celcius_to_farenheit(celcius: f32) -> f32{
+     (celcius * 9.0/5.0) + 32.0
+}
+
+fn farenheit_to_celcius(farenheit: f32) -> f32{
+    (farenheit - 32.0) * 5.0/9.0
+}
+
 fn main() {
-    println!("Temperature convertor");
+    let temp:f32;
 }
