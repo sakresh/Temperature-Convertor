9dabab499b8796ea887c01b3a99ea04b7ac032af
name <sarkeshnandu@gmail.com>
2024-04-01 21:38:31 +0530

    Ok it took so many attempts for me to get this damn thing right. Finally got it working. And now I know how to use custom types(ENUMS)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
-enum Type{
-    F(String),
-    C(String),
+use std::*;
+
+enum TempType{
+    F,
+    C,
 }
 
-fn celcius_to_farenheit(celcius: f32) -> f32{
-     (celcius * 9.0/5.0) + 32.0
-}
-
-fn farenheit_to_celcius(farenheit: f32) -> f32{
-    (farenheit - 32.0) * 5.0/9.0
+fn convert_temperature(temp: f32, temp_type: TempType) -> f32{
+    match temp_type{
+        TempType::F => (temp * 9.0/5.0) + 32.0,
+        TempType::C => (temp - 32.0) * 5.0/9.0,
+    }
 }
 
 fn main() {
-    let temp:f32;
+    println!("Enter the temperature in number: ");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read input!");
+    let num = input.trim().parse().expect("Failed to parse!");
+    println!("Enter the temperature type to be converted: ");
+    let mut temp_type = String::new();
+    io::stdin().read_line(&mut temp_type).expect("Failed to read input!");
+    let type_input = match temp_type.trim().to_uppercase().as_str() {
+        "F" => TempType::F,
+        "C" => TempType::C,
+        _ => panic!("Invalid input"),
+    };
+    let to_type = match type_input {
+        TempType::F => "Farenheit",
+        TempType::C => "Celcius",
+    };
+    let res = convert_temperature(num, type_input);
+    println!("{} in {} is {}", num, to_type, res );
 }
